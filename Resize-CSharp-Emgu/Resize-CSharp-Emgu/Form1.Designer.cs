c9bffee01123062f80b25ff017de739690bf08b5
Zhen <lizhen11@mails.tsinghua.edu.cn>
2015-01-11 15:01:52 +0800

    Basic UI for Resize-Project

---
+++ Resize-CSharp-Emgu/Resize-CSharp-Emgu/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Resize_CSharp_Emgu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelImg = new System.Windows.Forms.Panel();
+            this.panelBtn = new System.Windows.Forms.Panel();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.panelBtn.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelImg
+            // 
+            this.panelImg.Location = new System.Drawing.Point(20, 20);
+            this.panelImg.Name = "panelImg";
+            this.panelImg.Size = new System.Drawing.Size(500, 500);
+            this.panelImg.TabIndex = 0;
+            // 
+            // panelBtn
+            // 
+            this.panelBtn.Controls.Add(this.buttonLoad);
+            this.panelBtn.Location = new System.Drawing.Point(558, 20);
+            this.panelBtn.Name = "panelBtn";
+            this.panelBtn.Size = new System.Drawing.Size(100, 500);
+            this.panelBtn.TabIndex = 1;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(0, 77);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(100, 30);
+            this.buttonLoad.TabIndex = 0;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 553);
+            this.Controls.Add(this.panelBtn);
+            this.Controls.Add(this.panelImg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelBtn.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelImg;
+        private System.Windows.Forms.Panel panelBtn;
+        private System.Windows.Forms.Button buttonLoad;
+    }
+}
+
---
+++ Resize-CSharp-Emgu/Resize-CSharp-Emgu/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+
+namespace Resize_CSharp_Emgu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
